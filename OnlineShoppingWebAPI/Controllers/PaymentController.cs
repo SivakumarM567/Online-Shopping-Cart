c1de9ed7fdba573425063d2ef5eb07584cf8cd09
sivakumar <shivakumar85249@gmail.com>
2022-09-28 08:41:08 +0530

    Commit

---
+++ OnlineShoppingWebAPI/Controllers/PaymentController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShoppingWebAPI.Data;
+using OnlineShoppingWebAPI.Models;
+
+namespace OnlineShoppingWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ShoppingDbContext _context;
+
+        public PaymentController(ShoppingDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPaymentDetails()
+        {
+            var payments = await _context.Payments.ToListAsync();
+            return Ok(payments);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> GetPaymentDetailsById([FromRoute] int id)
+        {
+            var paymentDetails = await _context.Payments.FirstOrDefaultAsync(x => x.Paymentid == id);
+
+            if (paymentDetails != null)
+            {
+                return Ok(paymentDetails);
+            }
+            return NotFound("Payment Not Found");
+        }
+
+        [HttpPost]
+        [Route("addpayment")]
+        public async Task<IActionResult> AddPaymentDetails([FromBody] Payment payment)
+        {
+            if (_context.Payments == null)
+            {
+                return Problem("Payment Entity is Empty.");
+            }
+            _context.Payments.Add(payment);
+            await _context.SaveChangesAsync();
+
+            return Ok(payment);
+        }
+    }
+}
---
+++ OnlineShoppingWebAPI/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShoppingWebAPI.Data;
+using OnlineShoppingWebAPI.Models;
+
+namespace OnlineShoppingWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ShoppingDbContext _context;
+
+        public UserController(ShoppingDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserDetails()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> GetUserDetailsById([FromRoute] int id)
+        {
+            var userDetails = await _context.Users.FirstOrDefaultAsync(x => x.Userid == id);
+
+            if (userDetails != null)
+            {
+                return Ok(userDetails);
+            }
+            return NotFound("User Not Found");
+        }
+
+        [HttpPost]
+        [Route("adduser")]
+        public async Task<IActionResult> AddUserDetails([FromBody] User user)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("User Entity is Empty.");
+            }
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+
+        [HttpDelete]
+        [Route("deleteuser/{id:int}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] int id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var userDetails = await _context.Users
+                .FirstOrDefaultAsync(m => m.Userid == id);
+            if (userDetails == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(userDetails);
+            await _context.SaveChangesAsync();
+            return Ok(userDetails);
+        }
+    }
+}
